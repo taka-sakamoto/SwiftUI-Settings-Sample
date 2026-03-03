64fbf90adb8d5fb08320184fc26af7c6a6c7a3dc
taka-sakamoto <goodspeed855@gmail.com>
2026-03-03 13:14:39 +0900

    Update LoginView and RootView and SettingsModel and SettingsView

---
+++ Sample/RootView.swift
@@ -0,0 +1,18 @@
+//
+//  RootView.swift
+//  Sample
+//
+//  Created by Takayuki Sakamoto on 2026/03/03.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RootView()
+}
